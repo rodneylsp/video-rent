cd43ae15e3423133cb28c1b6382c3d3b096cd939
Rodney Pereira <rodneylsp@gmail.com>
2020-03-09 18:34:06 +0000

    Refactoring do teste. (parcial)

--- src/test/java/com/videorent/business/database/ClienteDAOTest.java
+++ src/test/java/com/videorent/business/database/ClienteDAOTest.java
@@ -7,30 +7,30 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 public class ClienteDAOTest {
 
+    @PersistenceContext
     private static EntityManager entityManager;
 
-    @BeforeClass
-    public static void init(){
-        entityManager = JPAUtil.getEntityManager();
-    }
+    @Inject
+    private DAO<Cliente> dao;
 
-    //@Test
+    @Test
     public void inserirClienteTest(){
 
         Cliente cliente = new Cliente();
         cliente.setId(2L);
-        cliente.setNome("Rodney");
+        cliente.setNome("Rodney Luiz da Silva Pereira");
 
-//        ClienteDAO dao = ClienteDAO.getInstance(entityManager);
-//        dao.insert(cliente);
-//
-//        Cliente clienteEncontrado = dao.findById(2L);
-//
-//        Assert.assertNotNull(clienteEncontrado);
-//        Assert.assertEquals("Rodney", clienteEncontrado.getNome());
+        dao.insert(cliente);
+
+        Cliente clienteEncontrado = dao.findById(2L);
+
+        Assert.assertNotNull(clienteEncontrado);
+        Assert.assertEquals("Rodney", clienteEncontrado.getNome());
     }
 }
