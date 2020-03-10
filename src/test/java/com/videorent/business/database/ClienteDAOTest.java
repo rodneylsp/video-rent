94ae52e52b8622d7e0b0644b2094f2ce71f0ba89
Rodney Pereira <rodneylsp@gmail.com>
2020-03-10 13:53:39 +0000

    Teste do ClienteDAO

--- src/test/java/com/videorent/business/database/ClienteDAOTest.java
+++ src/test/java/com/videorent/business/database/ClienteDAOTest.java
@@ -3,34 +3,62 @@ package com.videorent.business.database;
 import com.videorent.business.database.config.JPAUtil;
 import com.videorent.business.database.impl.ClienteDAO;
 import com.videorent.business.model.Cliente;
-import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class ClienteDAOTest {
 
-    @PersistenceContext
     private static EntityManager entityManager;
-
-    @Inject
     private DAO<Cliente> dao;
+
+    @Before
+    public void init(){
+        entityManager = JPAUtil.getEntityManager();
+        dao = new ClienteDAO(entityManager);
+    }
+
+    @After
+    public void destroy(){
+        entityManager.close();
+    }
 
     @Test
     public void inserirClienteTest(){
 
         Cliente cliente = new Cliente();
-        cliente.setId(2L);
+        cliente.setId(3L);
         cliente.setNome("Rodney Luiz da Silva Pereira");
 
         dao.insert(cliente);
+        Cliente clienteEncontrado = dao.findById(3L);
 
-        Cliente clienteEncontrado = dao.findById(2L);
-
-        Assert.assertNotNull(clienteEncontrado);
-        Assert.assertEquals("Rodney", clienteEncontrado.getNome());
+        assertNotNull(clienteEncontrado);
+        assertEquals("Rodney Luiz da Silva Pereira", clienteEncontrado.getNome());
     }
+
+    @Test
+    public void findAllTest(){
+
+        Cliente cliente = new Cliente();
+        cliente.setId(1L);
+        cliente.setNome("Rodney");
+        dao.insert(cliente);
+
+        cliente = new Cliente();
+        cliente.setId(2L);
+        cliente.setNome("Jose");
+        dao.insert(cliente);
+
+        List<Cliente> clientes = dao.findAll();
+        assertNotNull(clientes);
+        assertEquals(2, clientes.size());
+    }
+
 }
